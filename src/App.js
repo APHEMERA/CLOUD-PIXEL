a6e2bf89c071a1924b99bc0ceff9770db658d410
Roman <romann.romanchuk@gmail.com>
2024-01-06 14:38:04 +0100

    adding custom hook `useOnScreen`

--- src/App.js
+++ src/App.js
@@ -1,69 +1,13 @@
-import { useEffect, useState, useRef, useReducer } from "react";
 import { Fragment } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Gallery from "./components/Gallery";
 
-const reducer = (prevState, action) => {
-  if (action.type === "STATE_DATA") {
-    if (prevState.state != null && prevState) {
-      return {
-        state: [...prevState.state, action.state],
-      };
-    } else {
-      return {
-        state: ["", action.state],
-      };
-    }
-  }
-  return {
-    state: [],
-  };
-};
 function App() {
-  const [stateData, dispatch] = useReducer(reducer, { state: null });
-  const [page, setPage] = useState(1);
-  //console.log(stateData);
-  const url = `https://api.pexels.com/v1/search?query=nature&page=${page}`;
-  const fetchData = async () => {
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          Authorization: "J1liJnBCHbUnVkVNj2G5JjgNByv1ap9OzGE95rtw7oIhg1IhA2gRLO8G",
-          Accept: "application/json",
-        },
-      });
-      const data = await response.json();
-      console.log("data render");
-      dispatch({ type: "STATE_DATA", state: data });
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const observerRef = useRef(null);
-  console.log(observerRef.current);
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      const lastItem = entries[0];
-      if (!lastItem.isIntersecting) return;
-      observer.unobserve(lastItem.target);
-      console.log(lastItem.isIntersecting);
-      setPage((prevState) => prevState + 1);
-      fetchData();
-    });
-    if (observerRef.current) {
-      observer.observe(observerRef.current);
-    }
-  }, [observerRef]);
   return (
     <Fragment>
       <Header />
-      <Gallery data={stateData} ref={observerRef} />
+      <Gallery />
     </Fragment>
   );
 }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,9 +3,61 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faBookmark } from "@fortawesome/free-regular-svg-icons";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
-import { useState, forwardRef } from "react";
-const Gallery = forwardRef((props, ref) => {
+import { useEffect, useReducer, useState } from "react";
+import useOnScreen from "../hooks/useOnScreen";
+
+const reducer = (prevState, action) => {
+  if (action.type === "STATE_DATA") {
+    if (prevState.state != null && prevState) {
+      return {
+        state: [...prevState.state, action.state],
+      };
+    } else {
+      return {
+        state: ["", action.state],
+      };
+    }
+  }
+  return {
+    state: [],
+  };
+};
+const Gallery = () => {
   const [isHovering, setIsHovering] = useState(false);
+  const [stateData, dispatch] = useReducer(reducer, { state: null });
+  const [page, setPage] = useState(1);
+  const url = `https://api.pexels.com/v1/search?query=nature&page=${page}`;
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: "J1liJnBCHbUnVkVNj2G5JjgNByv1ap9OzGE95rtw7oIhg1IhA2gRLO8G",
+          Accept: "application/json",
+        },
+      });
+      const data = await response.json();
+      console.log("data render");
+      dispatch({ type: "STATE_DATA", state: data });
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const { measureRef, isIntersecting, observer } = useOnScreen();
+
+  useEffect(() => {
+    if (isIntersecting) {
+      setPage((prevState) => prevState + 1);
+      fetchData();
+      observer.disconnect();
+    }
+  }, [isIntersecting]);
+
   const handleMouseOver = () => {
     setIsHovering(true);
   };
@@ -15,12 +67,12 @@ const Gallery = forwardRef((props, ref) => {
   return (
     <div className={styles.parent}>
       <div className={styles.container}>
-        {props.data.state &&
-          props.data.state.map(
+        {stateData.state &&
+          stateData.state.map(
             (st) =>
               st &&
               st.photos.map((photo) => (
-                <a href="#" ref={ref}>
+                <a href="#" ref={measureRef}>
                   <div className={styles.photo} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
                     <img src={photo.src.portrait} className={styles.image} />
                     {isHovering && (
@@ -55,5 +107,5 @@ const Gallery = forwardRef((props, ref) => {
       </div>
     </div>
   );
-});
+};
 export default Gallery;
